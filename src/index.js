f216ac897dd74fe9e739d0e6899250fefd88c01e
kayjeee <kagiso.killagram@gmail.com>
2023-05-09 17:31:59 +0200

    Todo-list-structure with githubwork flow & linters

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import _ from 'lodash';
-import printMe from './print.js';
+
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
+function component() {
+  const element = document.createElement('div');
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+  return element;
+}
 
-  element.appendChild(btn);
-  element.classList.add('hello')
-   return element;
- }
-
- document.body.appendChild(component());
+document.body.appendChild(component());
